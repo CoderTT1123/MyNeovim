abe32381da029256c048d230c87dcc398147e569
CoderTT1123 <8tsu2a@gmail.com>
2023-01-12 20:51:51 +0900

    Shell script updated

--- initial.sh
+++ initial.sh
@@ -5,4 +5,9 @@ sudo apt upgrade
 sudo apt-get install xclip
 sudo apt-get install ripgrep
 sudo timedatectl set-timezone Asia/Seoul
+sudo apt install zsh
+sudo chsh -s `which zsh`
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+echo "Ended"
 
